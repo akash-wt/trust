46ce0b94cdf42a6af92fcbcb82b5635dc9a5c4e0
akash patel <akash906129@gmail.com>
2026-05-13 17:07:30 +0530

    changed main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,15 @@ use tun_tap::{Iface, Mode};
 fn main() -> io::Result<()> {
     let nic: Iface = Iface::new("tun0", Mode::Tun)?;
     let mut buf = [0u8; 1504];
-    loop{
-        let _nbytes = nic.recv(&mut buf[..])?;
-        eprintln!("{:?}", &buf[..52]);
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        eprintln!(
+            "flags {:?}, proto {:?} {:?} ",
+            flags,
+            proto,
+            &buf[4..nbytes]
+        );
     }
 }
