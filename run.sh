e95a7dc3a3ebd357dd686bf516b0ff6470610a62
akash <akash906129@gmail.com>
2026-05-13 20:18:06 +0530

    etherparse

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@ cargo b --release
 sudo setcap cap_net_admin=eip ~/trust/target/release/trust
 ~/trust/target/release/trust &
 pid=$!
- 
+
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
+
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,25 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        let eht_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800{
+            //not IPv4
+            continue;
+        }
+
+        match etherparse::IPv4HeaderSlice::from_slice(&[4..nbytes]){
+            Ok(p)=>{
+                eprintln("{}",p);
+            }
+            Err(e)=>{
+
+            }
+        }
         eprintln!(
             "flags {:x}, proto {:x} {:?} ",
-            flags,
-            proto,
+            eht_flags,
+            eth_proto,
             &buf[4..nbytes]
         );
 
